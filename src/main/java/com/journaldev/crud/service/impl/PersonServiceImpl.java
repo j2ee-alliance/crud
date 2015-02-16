3d4d3afa7df7e25ea79dc51a3662b7a76dcc0df7
Jurel Patoc <patocjurel@gmail.com>
2015-02-16 02:35:12 -0800

    j2ee initial commits

---
+++ src/main/java/com/journaldev/crud/service/impl/PersonServiceImpl.java
@@ -0,0 +1,54 @@
+package com.journaldev.crud.service.impl;
+
+import java.util.List;
+
+
+
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.journaldev.crud.dao.PersonDAO;
+import com.journaldev.crud.model.Person;
+import com.journaldev.crud.service.PersonService;
+
+@Service
+public class PersonServiceImpl implements PersonService{
+	
+	private PersonDAO personDAO;
+	
+	public void setPersonDAO(PersonDAO personDAO){
+		this.personDAO = personDAO;
+	}
+
+	@Override
+	@Transactional
+	public void addPerson(Person p) {
+		this.personDAO.addPerson(p);
+	}
+
+	@Override
+	@Transactional
+	public void updatePerson(Person p) {
+		this.personDAO.updatePerson(p);
+	}
+
+	@Override
+	@Transactional
+	public List<Person> listPersons() {
+		return this.personDAO.listPersons();
+	}
+
+	@Override
+	@Transactional
+	public Person getPersonById(int id) {
+		return this.personDAO.getPersonById(id);
+	}
+
+	@Override
+	@Transactional
+	public void removePerson(int id) {
+		this.personDAO.removePerson(id);
+	}
+}
